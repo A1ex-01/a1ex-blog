01a21fafa1f5e22d0e2d47ceddbbe3e40ea46321
a1ex <peterxhhx@gmail.com>
2024-10-31 22:51:02 +0800

    fix: add deploy

--- app/[lng]/post/[uuid]/page.tsx
+++ app/[lng]/post/[uuid]/page.tsx
@@ -13,26 +13,36 @@ export default async function page({ params: { uuid } }: pageProps) {
   if (!res?.success) notFound();
   const post: IPost = res.data;
   return (
-    <>
-      <div className="wrapper pb-10" style={{ backgroundImage: `url(${post?.notion?.cover})` }}>
-        <div className="topshow py-10 max-w-[1000px] text-white mx-auto">
-          <h2 className="text-3xl text-primary">{post.notion?.title}</h2>
+    <div
+      style={{ backgroundImage: `url(${post?.notion?.cover})` }}
+      className="min-w-screen bg-fixed overflow-hidden"
+    >
+      <div className="wrapper mt-10 bg-white rounded-lg p-4 max-w-[1000px] mx-auto">
+        <div className="topshow max-w-[1000px] mx-auto">
+          <h2 className="text-3xl text-primary font-bold">{post.notion?.title}</h2>
           <h3 className="text-lg mt-3">{post.notion?.title}</h3>
-          <Chip size="lg" color="primary">
+          <Chip size="lg" className="my-4" color="primary">
             {post.notion?.category?.name}
           </Chip>
+          <div className="tags flex gap-2">
+            {post.notion?.tags?.map((tag) => (
+              <Chip size="sm" key={tag.id}>
+                {tag.name}
+              </Chip>
+            ))}
+          </div>
         </div>
       </div>
       <article className="max-w-[1000px] py-4 mx-auto content">
-        <div className="md">
-          <Markdown className={"markdown-body"} remarkPlugins={[remarkGfm]}>
+        <div className="md rounded-lg p-4 bg-white/55">
+          <Markdown className={"markdown-body rounded-lg p-4"} remarkPlugins={[remarkGfm]}>
             {post.notion?.content}
           </Markdown>
         </div>
       </article>
-      <div className="mt-10 max-w-[1000px] mx-auto">
+      <div className="max-w-[1000px] mx-auto bg-white rounded-lg p-4 mb-4">
         <Waline />
       </div>
-    </>
+    </div>
   );
 }
