065d354b5f13f28e16eba8774a297102c5f89970
a1ex <peterxhhx@gmail.com>
2024-11-16 12:35:25 +0800

    feat: add codeFragments worker and add code hightlight

--- providers/index.tsx
+++ providers/index.tsx
@@ -1,16 +1,19 @@
 "use client";
 import { timeZone } from "@/config/global";
 import { ILocale } from "@/config/lng";
+import { ICodeFragment } from "@/types";
 import { arSA, deDE, enUS, esES, frFR, jaJP, koKR, ptPT, zhCN, zhTW } from "@clerk/localizations";
 import { ClerkProvider } from "@clerk/nextjs";
 import { NextUIProvider } from "@nextui-org/system";
 import { NextIntlClientProvider } from "next-intl";
 import * as React from "react";
 import ClientProvider from "./ClientProvider";
+import { CommonProvider } from "./CommonProvider";
 interface ProvidersProps {
   children: React.ReactNode;
   dicts: any;
   lng: ILocale;
+  codeFragments: ICodeFragment[];
 }
 const localeMap = {
   en: enUS,
@@ -24,12 +27,15 @@ const localeMap = {
   pt: ptPT,
   ar: arSA
 };
-export default function Providers({ children, dicts, lng }: ProvidersProps) {
+export default function Providers({ children, dicts, lng, codeFragments }: ProvidersProps) {
+  console.log("🚀 ~ Providers ~ codeFragments:", codeFragments);
   return (
     <ClerkProvider localization={localeMap[lng] ?? enUS}>
       <NextIntlClientProvider messages={dicts} locale={lng} timeZone={timeZone}>
         <NextUIProvider>
-          <ClientProvider>{children}</ClientProvider>
+          <ClientProvider>
+            <CommonProvider codeFragments={codeFragments}>{children}</CommonProvider>
+          </ClientProvider>
         </NextUIProvider>
       </NextIntlClientProvider>
     </ClerkProvider>
