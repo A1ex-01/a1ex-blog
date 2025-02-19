901d9a3daf511d64a19b7467cd5137753d2d6ec5
a1ex <peterxhhx@gmail.com>
2025-02-19 13:58:07 +0800

    feat: add markdown-it and shiki

--- app/[lng]/post/[uuid]/page.tsx
+++ app/[lng]/post/[uuid]/page.tsx
@@ -2,11 +2,11 @@ import { Badge } from "@/components/ui/badge";
 import { getMetadata } from "@/lib/metadata";
 import { getPost } from "@/services/common";
 import { IPost } from "@/services/types";
+import Shiki from "@shikijs/markdown-it";
 import "github-markdown-css";
+import MarkdownIt from "markdown-it";
 import { notFound } from "next/navigation";
 import { cache } from "react";
-import Markdown from "react-markdown";
-import remarkGfm from "remark-gfm";
 import Waline from "./_components/Waline";
 interface pageProps {}
 const getData = cache(async (uuid: string) => {
@@ -41,6 +41,19 @@ export default async function page({ params: { uuid } }: pageProps) {
   const res = await getData(uuid);
   if (!res?.success) notFound();
   const post: IPost = res.data;
+  const md: MarkdownIt = new MarkdownIt({
+    html: true, //
+    breaks: true,
+    typographer: true
+  });
+  md.use(
+    await Shiki({
+      themes: {
+        light: "vitesse-light"
+      }
+    })
+  );
+  const mdContent = md.render(post.notion.content);
   return (
     <div
       style={{ backgroundImage: `url(${post?.notion?.cover})` }}
@@ -65,10 +78,11 @@ export default async function page({ params: { uuid } }: pageProps) {
         </div>
       </div>
       <article className="max-w-7xl py-4 mx-auto content">
-        <div className="md rounded-lg p-4 bg-white/55">
-          <Markdown className={"markdown-body rounded-lg p-4"} remarkPlugins={[remarkGfm]}>
-            {post.notion?.content}
-          </Markdown>
+        <div className="md rounded-lg p-4 bg-white">
+          <div
+            className="markdown-body rounded-lg p-4"
+            dangerouslySetInnerHTML={{ __html: mdContent }}
+          />
         </div>
       </article>
       <div className="max-w-7xl mx-auto bg-white rounded-lg p-4 mb-4">
